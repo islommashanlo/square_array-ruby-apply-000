b1aa87594c0bd3e92365f5c70874b43313354bd6
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-14 19:45:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(numbers)
-  # your code here	  new_numbers = []
-end 	  numbers.each do |num|
+	  new_numbers = []
+    numbers.each do |num|
     new_numbers << (num ** 2)
   end
   return new_numbers
